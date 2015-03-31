dcf3d9166bf50efbe4ee701815efd0088e232d4f
Dan de Havilland <dan@dandehavilland.com>
2015-03-31 20:05:40 +0200

    Positioning & Scaling in place; extracted from ember component

--- src/leaflet-bubbles.js
+++ src/leaflet-bubbles.js
@@ -24,18 +24,6 @@ L.BubbleLayer = (L.Layer ? L.Layer : L.Class).extend({
     return this.redraw();
   },
 
-  setOptions: function (options) {
-    L.setOptions(this, options);
-    return this.redraw();
-  },
-
-  redraw: function () {
-    if (this._map && !this._frame && !this._map._animating) {
-      this._frame = L.Util.requestAnimFrame(this._redraw, this.options);
-    }
-    return this;
-  },
-
   onAdd: function (map) {
     this._map = map;
 
@@ -43,14 +31,7 @@ L.BubbleLayer = (L.Layer ? L.Layer : L.Class).extend({
       this._initSVG();
     }
 
-    // map._panes.overlayPane.appendChild(this._svg);
-
-    map.on('moveend', this._reset, this);
-
-    if (map.options.zoomAnimation && L.Browser.any3d) {
-      map.on('zoomanim', this._animateZoom, this);
-    }
-
+    map.on('moveend zoomend', this._reset, this);
     this._reset();
   },
 
@@ -58,11 +39,7 @@ L.BubbleLayer = (L.Layer ? L.Layer : L.Class).extend({
     d3.select(this._svg).remove();
     this._svg = null;
 
-    map.off('moveend', this._reset, this);
-
-    if (map.options.zoomAnimation) {
-      map.off('zoomanim', this._animateZoom, this);
-    }
+    map.off('moveend zoomend', this._reset, this);
   },
 
   addTo: function (map) {
@@ -73,7 +50,7 @@ L.BubbleLayer = (L.Layer ? L.Layer : L.Class).extend({
   _initSVG: function () {
     var svg = this._svg = d3.select(this._map._panes.overlayPane)
       .append('svg')
-      .classed('leaflet-bubble-layer', true)
+      .classed('leaflet-bubble-layer leaflet-zoom-hide', true)
       .node();
 
     var container = this._container = d3.select(svg)
@@ -91,7 +68,6 @@ L.BubbleLayer = (L.Layer ? L.Layer : L.Class).extend({
   */
   _reset: function () {
     var topLeft = this._map.containerPointToLayerPoint([0, 0]);
-    L.DomUtil.setPosition(this._svg, topLeft);
 
     var size = this._map.getSize();
     d3.select(this._svg).attr('width', size.x);
@@ -100,12 +76,19 @@ L.BubbleLayer = (L.Layer ? L.Layer : L.Class).extend({
     this._redraw();
   },
 
+  redraw: function() {
+    if (this._map) {
+      this._redraw();
+    }
+  },
+
   /**
     Update the bubble sizes and positions
   */
   _redraw: function () {
     var data = this._features,
       context = this,
+      svg = d3.select(this._svg),
       container = d3.select(this._container),
       join = container.selectAll('circle.bubble').data(data),
       bounds = this._map.getBounds(),
@@ -113,9 +96,19 @@ L.BubbleLayer = (L.Layer ? L.Layer : L.Class).extend({
       range = d3.scale.linear().domain(extent).range([0, this.options.maxRadius]),
       northWest = bounds.getNorthWest(),
       topLeft = this._applyLatLngToLayer(northWest.lng, northWest.lat),
-      bubbles;
+      southEast = bounds.getSouthEast(),
+      bottomRight = this._applyLatLngToLayer(southEast.lng, southEast.lat),
+    bubbles;
 
     container.attr('transform', 'translate('+ -topLeft.x +', '+ -topLeft.y +')');
+
+    svg.attr({
+      width: bottomRight.x - topLeft.x,
+      height: bottomRight.y - topLeft.y
+    }).style({
+      left: topLeft.x + 'px',
+      top: topLeft.y + 'px'
+    });
 
     join.exit();
 
@@ -123,36 +116,25 @@ L.BubbleLayer = (L.Layer ? L.Layer : L.Class).extend({
       .append('circle')
       .classed('bubble', true);
 
-    bubbles.attr({
-      cx: function(d) {
-        return context._applyLatLngToLayer(d[1], d[0]).x;
-      },
-      cy: function(d) {
-        return context._applyLatLngToLayer(d[1], d[0]).y;
-      }
-    })
-    // .transition().duration(500)
-    .attr('r', function(d) {
-      return range(d[2]);
-    });
+    container.selectAll('circle.bubble')
+      .attr({
+        cx: function(d) {
+          return context._applyLatLngToLayer(d[1], d[0]).x;
+        },
+        cy: function(d) {
+          return context._applyLatLngToLayer(d[1], d[0]).y;
+        }
+      })
+      // .transition().duration(500)
+      .attr('r', function(d) {
+        return range(d[2]);
+      });
 
     this._frame = null;
   },
 
   _applyLatLngToLayer: function(x, y) {
     return this._map.latLngToLayerPoint(new L.LatLng(y, x));
-  },
-
-  _animateZoom: function (e) {
-    var scale = this._map.getZoomScale(e.zoom),
-      offset = this._map._getCenterOffset(e.center)._multiplyBy(-scale).subtract(this._map._getMapPanePos());
-
-    if (L.DomUtil.setTransform) {
-       L.DomUtil.setTransform(this._svg, offset, scale);
-
-    } else {
-      this._svg.style[L.DomUtil.TRANSFORM] = L.DomUtil.getTranslateString(offset) + ' scale(' + scale + ')';
-    }
   }
 });
 
